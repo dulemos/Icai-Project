2959e7f7b2050c90e97717c3c5b3fd592b6cda75
Eduardo Santos <edu.l.santos.el@gmail.com>
2019-09-24 21:47:52 -0300

    Add JWT + Fix Login
    added JWT
    fix some logic at login problems

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,21 +1,21 @@
 module.exports.login = (app, req, res) =>  {
-    if(isValidEmail(req)){
-        let loginModel = app.models.login.userLogin(app, req, res)
-        
-        
-        
-        // console.log(loginModel)
-        // if(loginModel){
-        //     res.status(200).json(loginModel)
-        // }else{
-        //     res.status(404).json({"error": "Usuário não encontrado"})
-        // }
+    if(isValidEmail(req.body.email)){
+        let loginModel = app.models.login.userLogin( req.body.email, req.body.password, (token)=>{
+            if(token){
+                res.json({"token": token})
+            } else {
+                res.status(404).json({
+                    "status": "not found", 
+                    "error": "Usuário não encontrado"
+                })
+            }
+        })
     };
 }
 
 let isValidEmail = (email) => {
     let reg = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/;
-    if(reg.test(email.body.email)){
+    if(reg.test(email)){
         return true   
     }    
     return false
--- app/models/login.js
+++ app/models/login.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+
 
 let userModel = (app, req, res)=>{
     mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true});
@@ -14,16 +16,27 @@ let userModel = (app, req, res)=>{
         }
     });
     userSchema.set('autoIndex', false)
+    userSchema.set('toJSON', {virtuals: true})
     return mongoose.model('users', userSchema)
+}
+
+let authUser = (id, cb) => {
+    cb(jwt.sign({ id }, process.env.SECRET));
 }
 
 const user = userModel();
 
-module.exports.userLogin = async function (email){
-    user.find({'email': login}, (err, userData)=>{
-        console.log(`error: ${err}`)
-        console.log(`userData: ${userData}`)
-        if(err) return null; 
-        return userData
+module.exports.userLogin = function (email, pass, cb){
+    user.findOne({'user': email}, (err, userData)=>{
+        if(err) cb(null);
+        userData = userData.toJSON()
+        if(pass === userData.pass && pass){
+            authUser(userData._id, (token)=>{
+                cb(token)
+            })
+        }else{
+            cb(null);
+        }
     })
 }
+
